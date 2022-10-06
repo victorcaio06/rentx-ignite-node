f81f25be3c1854e637063aa0401796a8f90c0f50
Victor Caio <kaioaraujo426@gmail.com>
2022-10-06 09:16:18 -0300

    feat(categories): add uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
@@ -7,8 +8,8 @@ const categories = [];
 categoriesRoutes.post('/categories', (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({ name, description });
-  response.sendStatus(201);
+  categories.push({ id: uuidv4(), name, description });
+  return response.sendStatus(201);
 });
 
 export { categoriesRoutes };
