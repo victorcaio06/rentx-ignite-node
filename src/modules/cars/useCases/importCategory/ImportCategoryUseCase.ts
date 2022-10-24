8a04b5996312af5a4f3241459abc6dfa6c6a0dae
Victor Caio <kaioaraujo426@gmail.com>
2022-10-24 10:56:41 -0300

    FEAT(Categories): read file csv

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import fs from 'fs';
+import { parse } from 'csv-parse';
+
 export class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const csvFile = parse();
+    stream.pipe(csvFile);
+
+    csvFile.on('data', async (line) => {
+      console.log(line);
+    });
     console.log(file);
   }
 }
