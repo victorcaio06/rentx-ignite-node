bf9fd6ddfc98494c5328d109dfccb45e5a0c0255
Victor Caio <kaioaraujo426@gmail.com>
2023-01-14 13:42:58 -0300

    FEAT: added authentication user use case

---
+++ src/modules/accounts/useCases/authenticationUser/AuthenticationUserUseCase.ts
@@ -0,0 +1,48 @@
+import * as bcrypt from 'bcrypt';
+import * as jsonwebtoken from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+
+  token: string;
+}
+
+@injectable()
+export class AuthenticationUserUseCase {
+  private secretKey = process.env.SECRET_KEY;
+  constructor(
+    @inject('UsersRepository') private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    const token = jsonwebtoken.sign(
+      { user: user.name, email: user.email },
+      this.secretKey,
+      { subject: user.id, expiresIn: '1d' }
+    );
+
+    return { user, token };
+  }
+}
