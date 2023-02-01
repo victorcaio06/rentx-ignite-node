a34692490623183f5cc0df9fc9df7be34ac20a13
Victor Caio <kaioaraujo426@gmail.com>
2023-01-31 21:07:28 -0300

    REFACTOR: altered

--- src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -16,13 +16,10 @@ export class CategoriesRepositoryInMemory implements ICategoriesRepository {
   }
 
   async list(): Promise<Category[]> {
-    const categories = this.categories;
-
-    return categories;
+    return this.categories;
   }
 
   async findByName(name: string): Promise<Category> {
-    const category = this.categories.find((category) => category.name === name);
-    return category;
+    return this.categories.find((category) => category.name === name);
   }
 }
