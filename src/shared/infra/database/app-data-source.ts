502cf59354986b50195e014d1ff11e6940da929e
Victor Caio <kaioaraujo426@gmail.com>
2023-02-02 17:59:18 -0300

    ADDED AppDataSource and config for Server

---
+++ src/shared/infra/database/app-data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import { User } from '@modules/accounts/infra/typeorm/entities/User';
+import { Category } from '@modules/cars/infra/typeorm/entities/Category';
+import { Specification } from '@modules/cars/infra/typeorm/entities/Specification';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  port: 5432,
+  host: 'localhost',
+  username: 'docker',
+  password: 'ignite',
+  database: 'rentx',
+  synchronize: true,
+  entities: [Category, Specification, User],
+  migrations: ['./src/database/migrations/*.{ts,js}'],
+});
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import 'reflect-metadata';
