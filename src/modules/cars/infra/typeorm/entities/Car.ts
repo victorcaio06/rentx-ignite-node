dcf54cf60f95e3a8c12f17a1c6e547fcfe0eee52
Victor Caio <kaioaraujo426@gmail.com>
2023-02-07 13:12:17 -0300

    FEAT: added Car in database, delete old migrations

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,25 +1,50 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryColumn,
+} from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
+import { Category } from './Category';
 
+@Entity('cars')
 export class Car {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @Column()
   daily_rate: number;
 
+  @Column({ type: 'boolean', default: true })
   available: boolean;
 
+  @Column({ unique: true })
   license_plate: string;
 
+  @Column()
   fine_amount: number;
 
+  @Column()
   brand: string;
 
+  @CreateDateColumn()
   created_at?: Date;
 
-  category_id: string;
+  @ManyToMany(() => Category, (category) => category.id, {
+    nullable: true,
+    onDelete: 'SET NULL',
+    onUpdate: 'SET NULL',
+  })
+  @JoinTable()
+  category_id: Category[];
 
   constructor() {
     if (!this.id) {
--- src/shared/infra/typeorm/app-data-source.ts
+++ src/shared/infra/typeorm/app-data-source.ts
@@ -1,4 +1,6 @@
 import { DataSource } from 'typeorm';
+
+import { Car } from '../../../modules/cars/infra/typeorm/entities/Car';
 import { User } from '../../../modules/accounts/infra/typeorm/entities/User';
 import { Category } from '../../../modules/cars/infra/typeorm/entities/Category';
 import { Specification } from '../../../modules/cars/infra/typeorm/entities/Specification';
@@ -11,6 +13,6 @@ export const AppDataSource = new DataSource({
   password: 'ignite',
   database: 'rentx',
   synchronize: true,
-  entities: [Category, Specification, User],
+  entities: [Specification, User, Category, Car],
   migrations: ['./src/shared/infra/typeorm/migrations/*.{ts,js}'],
 });
