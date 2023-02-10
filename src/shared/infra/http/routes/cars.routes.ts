c84d3f4bd63e51e4d7e0ebc6442c7633821e7605
Victor Caio <kaioaraujo426@gmail.com>
2023-02-09 21:19:46 -0300

    FEAT: created middleware for verify if user is a admin

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 
-carsRoutes.post('/create', createCarController.handle);
+carsRoutes.post(
+  '/create',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle
+);
 
 export { carsRoutes };
